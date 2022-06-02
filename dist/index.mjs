4c4de91284955d04f3a29d2c276c835260c6671e
Jonas Strassel <jo.strassel@gmail.com>
2022-06-02 13:47:50 +0200

    feat: pass default values into createClient

--- dist/index.mjs
+++ dist/index.mjs
@@ -1,9 +1,26 @@
 var __create = Object.create;
 var __defProp = Object.defineProperty;
+var __defProps = Object.defineProperties;
 var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropDescs = Object.getOwnPropertyDescriptors;
 var __getOwnPropNames = Object.getOwnPropertyNames;
+var __getOwnPropSymbols = Object.getOwnPropertySymbols;
 var __getProtoOf = Object.getPrototypeOf;
 var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __propIsEnum = Object.prototype.propertyIsEnumerable;
+var __defNormalProp = (obj, key, value) => key in obj ? __defProp(obj, key, { enumerable: true, configurable: true, writable: true, value }) : obj[key] = value;
+var __spreadValues = (a, b) => {
+  for (var prop in b || (b = {}))
+    if (__hasOwnProp.call(b, prop))
+      __defNormalProp(a, prop, b[prop]);
+  if (__getOwnPropSymbols)
+    for (var prop of __getOwnPropSymbols(b)) {
+      if (__propIsEnum.call(b, prop))
+        __defNormalProp(a, prop, b[prop]);
+    }
+  return a;
+};
+var __spreadProps = (a, b) => __defProps(a, __getOwnPropDescs(b));
 var __require = /* @__PURE__ */ ((x) => typeof require !== "undefined" ? require : typeof Proxy !== "undefined" ? new Proxy(x, {
   get: (a, b) => (typeof require !== "undefined" ? require : a)[b]
 }) : x)(function(x) {
@@ -11,8 +28,24 @@ var __require = /* @__PURE__ */ ((x) => typeof require !== "undefined" ? require
     return require.apply(this, arguments);
   throw new Error('Dynamic require of "' + x + '" is not supported');
 });
+var __objRest = (source, exclude) => {
+  var target = {};
+  for (var prop in source)
+    if (__hasOwnProp.call(source, prop) && exclude.indexOf(prop) < 0)
+      target[prop] = source[prop];
+  if (source != null && __getOwnPropSymbols)
+    for (var prop of __getOwnPropSymbols(source)) {
+      if (exclude.indexOf(prop) < 0 && __propIsEnum.call(source, prop))
+        target[prop] = source[prop];
+    }
+  return target;
+};
 var __commonJS = (cb, mod) => function __require2() {
   return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
+};
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
 };
 var __copyProps = (to, from, except, desc) => {
   if (from && typeof from === "object" || typeof from === "function") {
@@ -2866,4 +2899,20 @@ var require_client = __commonJS({
 
 // src/index.ts
 var src_exports = {};
+__export(src_exports, {
+  createClient: () => createClient
+});
+var import_client = __toESM(require_client());
 __reExport(src_exports, __toESM(require_client()));
+var createClient = (_a) => {
+  var _b = _a, { apiKey } = _b, options = __objRest(_b, ["apiKey"]);
+  options.batch = !!options.batch ? options.batch : false;
+  options.url = options.url ?? "https://transit.land/api/v2/query";
+  options.headers = __spreadProps(__spreadValues({}, options.headers), {
+    apiKey
+  });
+  return (0, import_client.createClient)(options);
+};
+export {
+  createClient
+};
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-export * from './_client'
+import type { ClientOptions as OriginalClientOptions} from '@genql/runtime';
+import { Optional } from 'utility-types';
+import { createClient as originalCreateClient } from './_client';
+
+type ClientOptions = Optional<OriginalClientOptions, 'url' | 'batch'> & {
+    apiKey: string;
+};
+
+export const createClient = ({ apiKey, ...options }: ClientOptions) => {
+    options.batch = !!options.batch ? options.batch : false;
+    options.url = options.url ?? "https://transit.land/api/v2/query";
+    options.headers = {
+        ...options.headers,
+        apiKey
+    };
+    return originalCreateClient(options);
+}
+export * from './_client';
